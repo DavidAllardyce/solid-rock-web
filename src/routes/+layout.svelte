5d585b815595acc1abde2199a8c69f6d232b10c5
David Allardyce <daveallardyce@gmail.com>
2023-08-20 12:48:28 -0400

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+
+<script>
+  import "../app.css";
+</script>
+  
+  <nav class="flex items-center justify-between flex-wrap bg-white-500 p-6">
+    <div class="flex items-center flex-shrink-0  mr-6">
+      <img class="fill-current h-8 w-auto mr-2" src="logo-350x68-high-res.png" alt="solid-rock-logo" />
+    </div>
+    <div class="block lg:hidden">
+      <button class="flex items-center px-3 py-2 border rounded text-white-200 border-white-400 hover:text-blue-500 hover:border-blue-500">
+        <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+      </button>
+    </div>
+    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+      <div class="text-sm lg:flex-grow">
+        <a href="/" class="block mt-4 lg:inline-block lg:mt-0 text-white-200 hover:text-sky-700 mr-4 uppercase font-bold">
+          Home
+        </a>
+        <a href="/mission" class="block mt-4 lg:inline-block lg:mt-0 text-white-200 hover:text-sky-700 mr-4 uppercase font-bold">
+          Mission
+        </a>
+        <a href="/staff" class="block mt-4 lg:inline-block lg:mt-0 text-white-200 hover:text-sky-700 uppercase font-bold">
+          Meet the Staff
+        </a>
+      </div>
+      <div>
+        <a href="/login" class="inline-block text-sm px-4 py-2 leading-none border rounded text-teal-900 border-teal-900 hover:border-transparent hover:text-sky-500 hover:bg-stone-50 hover:border-sky-500 mt-4 lg:mt-0">Sign In</a>
+      </div>
+    </div>
+  </nav>
+
+<slot></slot>
+
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    /** @type {import('./$types').PageData} */
+    export let data;
+</script>
+
+  <div class="relative overflow-hidden bg-cover bg-no-repeat" style="
+        background-position: 50%;
+        background-image: url('church_front_sm.jpg');
+        height: 500px;
+      ">
+    <div
+      class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsla(0,0%,0%,0.75)] bg-fixed">
+      <div class="flex h-full items-center">
+        <div class="px-6 text-center text-white md:px-12 justify-left">
+          <h1 class="mt-2 mb-16 text-3xl font-bold tracking-tight md:text-5xl xl:text-7xl uppercase">
+            Home
+          </h1>
+          </div>
+          <div class="justify-center">
+          <button type="button"
+            class="rounded border-2 border-neutral-50 px-[46px] pt-[14px] pb-[12px] text-sm font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-100 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200"
+            data-te-ripple-init data-te-ripple-color="light">
+            Join Us
+          </button>
+        </div>
+      </div>
+  </div>
+</div>
